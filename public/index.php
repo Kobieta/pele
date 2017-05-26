147bfc0c706255857dd36fa79b383b2879020594
pbilka46 <patryk.bilka@gmail.com>
2017-05-26 13:25:53 +0200

    HTTP authentication

--- public/index.php
+++ public/index.php
@@ -1,60 +1,72 @@
 <?php
 
-header('S-Host: ' . $_SERVER['HTTP_HOST']);
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+if( ( isset($_SERVER['PHP_AUTH_USER'] ) && ( $_SERVER['PHP_AUTH_USER'] == "admin" ) ) AND
+    ( isset($_SERVER['PHP_AUTH_PW'] ) && ( $_SERVER['PHP_AUTH_PW'] == "b123KOPqw0_kso1" )) ) {
+    /**
+     * Laravel - A PHP Framework For Web Artisans
+     *
+     * @package  Laravel
+     * @author   Taylor Otwell <taylor@laravel.com>
+     */
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We'll simply require it
-| into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels nice to relax.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Register The Auto Loader
+    |--------------------------------------------------------------------------
+    |
+    | Composer provides a convenient, automatically generated class loader for
+    | our application. We just need to utilize it! We'll simply require it
+    | into the script here so that we don't have to worry about manual
+    | loading any of our classes later on. It feels nice to relax.
+    |
+    */
 
-require __DIR__.'/../bootstrap/autoload.php';
+    require __DIR__.'/../bootstrap/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| We need to illuminate PHP development, so let us turn on the lights.
-| This bootstraps the framework and gets it ready for use, then it
-| will load up this application so that we can run it and send
-| the responses back to the browser and delight our users.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Turn On The Lights
+    |--------------------------------------------------------------------------
+    |
+    | We need to illuminate PHP development, so let us turn on the lights.
+    | This bootstraps the framework and gets it ready for use, then it
+    | will load up this application so that we can run it and send
+    | the responses back to the browser and delight our users.
+    |
+    */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    $app = require_once __DIR__.'/../bootstrap/app.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Run The Application
+    |--------------------------------------------------------------------------
+    |
+    | Once we have the application, we can handle the incoming request
+    | through the kernel, and send the associated response back to
+    | the client's browser allowing them to enjoy the creative
+    | and wonderful application we have prepared for them.
+    |
+    */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
 
-$response->send();
+    $response->send();
 
-$kernel->terminate($request, $response);
+    $kernel->terminate($request, $response);
+}
+
+else {
+    header("WWW-Authenticate: " .
+        "Basic realm=\"Leon's Protected Area\"");
+    header("HTTP/1.0 401 Unauthorized");
+
+    //Show failure text, which browsers usually
+    //show only after several failed attempts
+    print("This page is protected.");
+}
